5b97dca0a2a5f52395bba4b5eab04916bf544785
DelTa-0 <aryalmadhave123@gmail.com>
2025-02-16 18:45:29 +0545

    register user

--- MeroBazar/store/views.py
+++ MeroBazar/store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,redirect
 from .models import Product
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 # Create your views here.
 def home(request):
@@ -13,12 +17,35 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 
+
+def register_user(request):
+    form=SignUpForm()
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,("you have registered!!"))
+            return redirect('home')
+        else:
+            messages.success(request,("Cannot register try again"))
+            return redirect('register')
+    else:
+        return render(request,'register.html',{
+            'form':form
+        })
+    
+
 def login_user(request):
     if request.method=='POST':
         print(request.POST)
         username=request.POST['username']
         password=request.POST['password']
         user=authenticate(request,username=username,password=password)
+        
         if user is not None:
             login(request,user)
             messages.success(request,("you have logged in!"))
@@ -29,6 +56,7 @@ def login_user(request):
     else:
         
         return render(request,'login.html')
+    
 
     
 
